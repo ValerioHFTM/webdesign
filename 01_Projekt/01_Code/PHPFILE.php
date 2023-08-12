d5e6250ba7bae3a1823f851e466d3c43ce9592c3
Valerio Bottinelli <valerio.bottinelli@hftm.ch>
2023-08-12 10:56:49 +0200

    Initial checking

---
+++ 01_Projekt/01_Code/PHPFILE.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+ <title> <?php echo "Hallo Welt!"; ?> </title>
+ </head>
+ <body>
+ <h1>Adressen</h1>
+ <ul>
+ <?php
+ for ($i = 0; $i < 10; $i++) {
+ echo " <li>$i</li>\n";
+ }
+ ?>
+ </ul>
+ </body>
+</html>
