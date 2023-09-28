7a7bb7ff9057343ef05fc9fccdb7666a1b37e05e
valerio Bottinelli Main <valerio.bottinelli@hftm.ch>
2023-09-29 00:26:22 +0200

    snowboard update

---
+++ 01_Projekt/01_Code/Snowboard_content.php
@@ -0,0 +1,65 @@
+<html>
+    <div class="container">
+        <h2>Snowboard NITRO</h2>
+        <p>
+          Go to<a
+            href="https://nitrosnowboards.com/de"
+          >
+            nitrosnowboards.com</a
+          >
+        </p>
+        <h5>Supplier Description</h5>
+        <p class="manufacturerDescription">
+        The Nitro Team Splitboard allows everyone to enjoy regular chairlift days at the ski resort on a comfortable and versatile snowboard, 
+        but with the added advantage of being able to venture beyond the marked slopes to reach remote peaks. 
+        This means that Team Splitboarders can leave the resort on a busy weekend and embark on a backcountry tour in search of fresh descents and new perspectives. 
+        The Team Split's Dual Degressive Sidecut, Directional Twin Shape, and progressive widths ensure that this board feels like a normal resort cruiser. 
+        The Trüe Camber and Standard Core Profile provide you with the necessary grip and support for uphill and downhill touring.
+<br>
+<br>
+Equipped with Nitro's robust Tail and Tip Locks, forged T7075 aluminum hooks, and the Nitro x Kohla Attachment System, 
+you can switch to Splitboarding Hike Mode whenever you feel like it. 
+Just remember to have your backcountry gear and the specially tailored Nitro x Kohla Peak Skins in your backpack.
+<br>
+<br>
+In the past, you had to choose between buying a new Splitboard or a regular snowboard. 
+Now, you no longer need to invest in both or choose between a Splitboard or a resort snowboard setup. 
+You can finally get a versatile all-mountain board that does it all - the new Nitro Team Split!
+        </p>
+        <div >
+        <img
+          src="../02_Images/Snowboard/Board_Action2.jpg"
+          alt="MSR Access 3"
+          class="product"
+        >
+        </div>
+        <h4 class="summary">Summary: 10 / 10</h4>
+     
+        <h4>The Review in details</h4>
+    <h5>Size</h5>
+    <p>
+      The Nitro Cinema Felt great on my legs. It is light which makes a feeling that I have nothing attached to my legs.
+      The size 159 fits my weight well stance is very comfortable as it's natural, I have no problem with turns in it,
+      although I'm used to smaller sizes.
+    </p>
+    <h5>Flex</h5>
+    <p>
+      Very good flex, it's in my opinion the better ones I tried. <b>It's not too soft yet it's not crazy stiff </b>.
+      Really a
+      pleasant pop and a great response to tearing up the slope.
+    </p>
+    <h5>On snow feel</h5>
+    <p>
+      So easy to ride and so forgiving for those that skid turns or just want a ride that is less likely than camber to
+      catch an edge. Like all hybrid rocker boards, it has that semi-stable feel underfoot that wants to auto-spin one
+      footing or flat basing in harder snow but it becomes pretty stable in softer snow.
+    </p>
+    <h5>Turning experience</h5>
+    <p>
+      Again it's really good, it can edge really nice, but also depends on how "how low can you go" and "if you're brave
+      enough". Speed and your own limits are key here. I'm not having years of expereience but
+      the turning experience was much better than competition.
+    </p>
+    <h5>Find your size</h5>
+</html>
+
